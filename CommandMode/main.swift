e5519f489942e53398651a1abda19ebd8308ab53
phineas_huang <phineas_huang@gemteks.com>
2017-01-16 09:47:54 +0800

    Initial Commit

---
+++ CommandMode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CommandMode
+//
+//  Created by Phineas_Huang on 16/01/2017.
+//  Copyright © 2017 SunXiaoShan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
